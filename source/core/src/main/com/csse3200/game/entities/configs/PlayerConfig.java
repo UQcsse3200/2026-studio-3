1a91bc1698cdd50e82654107620f4b4374df991b
Celeste116 <sixian.cheng@student.uq.edu.au>
2026-08-28 19:51:40 +1000

    feat: wire maxHealth and EnergyComponent into PlayerFactory

--- source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
@@ -3,5 +3,6 @@ package com.csse3200.game.entities.configs;
 /** Defines the properties stored in player config files to be loaded by the Player Factory. */
 public class PlayerConfig extends BaseEntityConfig {
   public int gold = 1;
+  public int maxEnergy = 3;
   public String favouriteColour = "none";
 }
