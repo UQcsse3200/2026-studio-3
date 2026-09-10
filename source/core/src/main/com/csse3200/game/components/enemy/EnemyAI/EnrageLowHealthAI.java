fc647368fcbe073ef3182f633f9276ad117b7f3a
xiaojaxon <xiaojaxon@gmail.com>
2026-09-11 01:14:16 +1000

    add more logic to second elit enemy

--- source/core/src/main/com/csse3200/game/components/enemy/EnemyAI/EnrageLowHealthAI.java
+++ source/core/src/main/com/csse3200/game/components/enemy/EnemyAI/EnrageLowHealthAI.java
@@ -1,6 +1,7 @@
 package com.csse3200.game.components.enemy.EnemyAI;
 
 import com.csse3200.game.components.enemy.EnemyIntent;
+import com.csse3200.game.components.enemy.IntentType;
 import java.util.Objects;
 
 /**
@@ -10,22 +11,22 @@ import java.util.Objects;
  * enraged, doubling the damage of every attack for the rest of the fight.
  */
 public class EnrageLowHealthAI implements EnemyAI {
-  private static final int DEFEND_AMOUNT = 3;
   private static final float ENRAGE_THRESHOLD = 0.5f;
   private static final int ENRAGE_MULTIPLIER = 2;
+
+  private final EnemyAI basePattern = new CycleFourStanceAI();
 
   @Override
   public EnemyIntent decide(EnemyAIContext context) {
     Objects.requireNonNull(context, "context cannot be null");
 
-    if (context.getTurnNumber() % 2 == 0) {
-      return EnemyIntent.defend(DEFEND_AMOUNT);
+    EnemyIntent baseIntent = basePattern.decide(context);
+
+    if (context.getEnemyHealthRatio() < ENRAGE_THRESHOLD
+            && baseIntent.getType() == IntentType.ATTACK) {
+      return EnemyIntent.attack(baseIntent.getValue() * ENRAGE_MULTIPLIER);
     }
 
-    int damage = context.getEnemyAttack();
-    if (context.getEnemyHealthRatio() < ENRAGE_THRESHOLD) {
-      damage *= ENRAGE_MULTIPLIER;
-    }
-    return EnemyIntent.attack(damage);
+    return baseIntent;
   }
 }
