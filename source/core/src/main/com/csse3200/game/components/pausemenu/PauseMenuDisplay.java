19a6036193e350bb40c75f18d43395e793457131
Sahil <sahhh0534@gmail.com>
2026-09-13 17:43:00 +1000

    Block gameplay input while game is paused

--- source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuDisplay.java
@@ -5,10 +5,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.csse3200.game.ui.UIComponent;
@@ -69,6 +72,19 @@ public class PauseMenuDisplay extends UIComponent {
     table = new Table();
     table.setFillParent(true);
     table.setBackground(new TextureRegionDrawable(new TextureRegion(createBackgroundTexture())));
+    table.setTouchable(Touchable.enabled);
+    table.addListener(
+        new InputListener() {
+          @Override
+          public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+            return table.isVisible();
+          }
+
+          @Override
+          public boolean scrolled(InputEvent event, float x, float y, float amountX, float amountY) {
+            return table.isVisible();
+          }
+        });
     table.center();
 
     resumeButton = new TextButton("Resume", skin);
@@ -273,5 +289,9 @@ public class PauseMenuDisplay extends UIComponent {
 
   TextButton getSettingsBackButton() {
     return settingsBackButton;
+  }
+
+  Table getRootTable() {
+    return table;
   }
 }
--- source/core/src/test/com/csse3200/game/components/pausemenu/PauseMenuInputTest.java
+++ source/core/src/test/com/csse3200/game/components/pausemenu/PauseMenuInputTest.java
@@ -3,10 +3,14 @@ package com.csse3200.game.components.pausemenu;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.badlogic.gdx.Input.Keys;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.GamePauseService;
+import com.csse3200.game.services.ServiceLocator;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -42,5 +46,22 @@ class PauseMenuInputTest {
 
     assertFalse(handled);
     assertEquals(0, pauseCount.get());
+  }
+
+  @Test
+  void otherKeysAreConsumedWhilePaused() {
+    GamePauseService pauseService = mock(GamePauseService.class);
+    when(pauseService.isPaused()).thenReturn(true);
+    ServiceLocator.registerPauseService(pauseService);
+
+    assertTrue(input.keyDown(Keys.SPACE));
+    assertTrue(input.keyTyped('a'));
+    assertTrue(input.keyUp(Keys.SPACE));
+    assertEquals(0, pauseCount.get());
+  }
+
+  @Test
+  void hasPriorityAboveGameplayAndDebugInputs() {
+    assertTrue(input.getPriority() > 20);
   }
 }
