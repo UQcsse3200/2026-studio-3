3d701362a512a41beba9ab9f94cbb11eda85cfe8
YU ZHU <pigfish668@gmail.com>
2026-09-13 17:10:40 +1000

    Add PIERCE effect to Poison Blade

--- source/core/src/main/com/csse3200/game/cards/EffectType.java
+++ source/core/src/main/com/csse3200/game/cards/EffectType.java
@@ -19,7 +19,9 @@ public enum EffectType {
   /** Decreases 25% the damage output to target while active. */
   FEEBLE(true),
   /** Increases damage the target deals for the rest of the combat. */
-  STRENGTH(false);
+  STRENGTH(false),
+  /** Immediate damage that ignores the target's block and armour. */
+  PIERCE(false);
 
   private final boolean usesDuration;
 
