f9ee9f382f0e82ac0668e8fd10878d7205870f7f
Celeste116 <sixian.cheng@student.uq.edu.au>
2026-09-22 15:48:32 +1000

    Add inventory popup UI and wire up shop discount calculation

---
+++ source/core/src/main/com/csse3200/game/components/battle/InventoryPopupComponent.java
@@ -0,0 +1,4 @@
+package com.csse3200.game.components.battle;
+
+public class InventoryPopupComponent {
+}
---
+++ source/core/src/main/com/csse3200/game/components/spritedisplay/displaying/InventoryDisplay.java
@@ -0,0 +1,4 @@
+package com.csse3200.game.components.spritedisplay.displaying;
+
+public class InventoryDisplay {
+}
