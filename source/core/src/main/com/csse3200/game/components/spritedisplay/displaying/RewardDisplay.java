4abe361226a790a1c574556c2add5598261ba5be
Keira0206 <keyue.lu@student.uq.edu.au>
2026-09-22 20:27:58 +1000

    Revert "refactor: expand RewardDisplay item handling and improve UI reward text formatting"
    This reverts commit 3941c846208a118947688eeb5c0f195c6982b485.

--- source/core/src/main/com/csse3200/game/components/spritedisplay/displaying/RewardDisplay.java
+++ source/core/src/main/com/csse3200/game/components/spritedisplay/displaying/RewardDisplay.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.csse3200.game.maps.PlayerRunState;
 import com.csse3200.game.maps.RunState;
 import com.csse3200.game.rewards.RewardOption;
 import com.csse3200.game.rewards.RewardService;
@@ -34,20 +33,12 @@ public class RewardDisplay extends Displaying {
   }
 
   private void buildOptionButtons() {
-    if (options == null || options.isEmpty()) {
-      return;
-    }
-
     Table optionsTable = new Table();
     optionsTable.setFillParent(true);
     optionsTable.top();
     optionsTable.padTop(400f);
 
     for (RewardOption option : options) {
-      if (option == null) {
-        continue;
-      }
-
       TextButton button = new TextButton(describeOption(option), skin);
       button.addListener(
           new ChangeListener() {
@@ -69,7 +60,7 @@ public class RewardDisplay extends Displaying {
               return true;
             }
           });
-      optionsTable.add(button).pad(10f).width(320f).height(80f);
+      optionsTable.add(button).pad(10f).width(300f).height(80f);
       optionsTable.row();
     }
 
@@ -77,91 +68,18 @@ public class RewardDisplay extends Displaying {
   }
 
   private String describeOption(RewardOption option) {
-    if (option == null) {
-      return "";
-    }
     return switch (option.type) {
-      case GOLD -> {
-        float multiplier = 0f;
-        if (runState != null && runState.getOrCreatePlayerState() != null) {
-          multiplier = runState.getOrCreatePlayerState().getGoldBonusMultiplier();
-        }
-        int finalGold = Math.round(option.goldAmount * (1 + multiplier));
-        yield multiplier > 0
-            ? finalGold + " Gold (+" + Math.round(multiplier * 100) + "%)"
-            : finalGold + " Gold";
-      }
-      case ITEM -> {
-        if (option.itemId == null) {
-          yield "Item";
-        }
-        yield switch (option.itemId) {
-          case LUCKY_COIN -> "Lucky Coin (+10% Gold)";
-          case MERCHANTS_FAVOR -> "Merchant's Favor (10% Off)";
-          case ENERGY_CRYSTAL -> "Energy Crystal (+20 HP)";
-        };
-      }
+      case GOLD -> option.goldAmount + " Gold";
+      case ITEM -> "Item: " + option.itemId;
     };
   }
 
   private void claimOption(RewardOption option) {
-    if (claimed || option == null) {
+    if (claimed) {
       return;
     }
     claimed = true;
-
-    if (runState != null) {
-      PlayerRunState playerState = runState.getOrCreatePlayerState();
-      if (playerState != null) {
-        switch (option.type) {
-          case GOLD -> {
-            int finalGold =
-                Math.round(option.goldAmount * (1 + playerState.getGoldBonusMultiplier()));
-            playerState.restore(
-                playerState.getCurrentHealth(),
-                playerState.getMaxHealth(),
-                playerState.getGold() + finalGold,
-                playerState.getGoldBonusMultiplier(),
-                playerState.getShopDiscount());
-          }
-          case ITEM -> {
-            if (option.itemId != null) {
-              switch (option.itemId) {
-                case LUCKY_COIN ->
-                    playerState.restore(
-                        playerState.getCurrentHealth(),
-                        playerState.getMaxHealth(),
-                        playerState.getGold(),
-                        playerState.getGoldBonusMultiplier() + 0.1f,
-                        playerState.getShopDiscount());
-
-                case MERCHANTS_FAVOR ->
-                    playerState.restore(
-                        playerState.getCurrentHealth(),
-                        playerState.getMaxHealth(),
-                        playerState.getGold(),
-                        playerState.getGoldBonusMultiplier(),
-                        playerState.getShopDiscount() + 0.1f);
-
-                case ENERGY_CRYSTAL -> {
-                  int healAmount = 20;
-                  int newHealth =
-                      Math.min(
-                          playerState.getCurrentHealth() + healAmount, playerState.getMaxHealth());
-                  playerState.restore(
-                      newHealth,
-                      playerState.getMaxHealth(),
-                      playerState.getGold(),
-                      playerState.getGoldBonusMultiplier(),
-                      playerState.getShopDiscount());
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
+    runState.setPendingReward(option);
     entity.getEvents().trigger(REWARD_CLAIMED_EVENT);
     entity.getEvents().trigger(EndBattleDisplay.RETURN_TO_MENU_EVENT);
   }
